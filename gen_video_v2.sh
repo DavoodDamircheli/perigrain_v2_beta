548bd5143b3e51d24430366d3fadfeabf24eca81
Davood <dd1424@msstate.edu>
2024-10-07 17:23:27 -0500

    generating the video form n1 to n2th images

--- gen_video_v2.sh
+++ gen_video_v2.sh
@@ -1,32 +1,29 @@
-#!/bin/bash
-dir=${1}
-output_dir=${2}
-n=${3}  # This is the variable n to control how many images to process
+#!/bin/bash                                                                                 
+dir=${1}                                                                                    
+output_dir=${2}                                                                             
+n1=${3}  # Starting image number (e.g., 1)                                                  
+n2=${4}  # Ending image number (e.g., 100)                                                  
+                                                                                            
+# scale while making video from pic                                                         
+scale=0.5                                                                                   
+IMG_PREF='img_'                                                                             
 
-# scale while making video from pic
-scale=0.5
-IMG_PREF='img_'
+RNDN=$(date +%F-%H-%M-%S)-$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 5)                 
 
-RNDN=$(date +%F-%H-%M-%S)-$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 5)
+FILE=${output_dir}/${RNDN}.mp4                                                              
 
+# Calculate the number of frames (n2 - n1 + 1)                                              
+frames=$(($n2 - $n1 + 1))                                                                   
 
+# standard                                                                                  
+framerate=40                                                                                
+r=25                                                                                        
+crf=30                                                                                      
 
+# Create video from images starting at n1 and ending at n2                                  
+ffmpeg -framerate $framerate -start_number $n1 -i ${dir}/${IMG_PREF}%5d.png -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -frames:v $frames -r $r -crf $crf ${FILE}                                    
 
-FILE=${output_dir}/${RNDN}.mp4
-
-# standard
-# framerate=20
-# r=25
-# crf=30
-
-framerate=40
-r=25
-crf=30
-
-# Pass the variable n to limit the number of frames
-ffmpeg -framerate $framerate -i ${dir}/${IMG_PREF}%5d.png -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -frames:v $n -r $r -crf $crf ${FILE}
-
-mplayer $FILE
+mplayer $FILE                                                                               
 
 echo $FILE
 
