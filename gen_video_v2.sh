2255f73c41cc19583e92730d5bd5087384b0d3bd
Davood <dd1424@msstate.edu>
2024-10-07 17:17:33 -0500

    the video can have different input and output directory

--- gen_video_v2.sh
+++ gen_video_v2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-OUTPUT_DIR=${1}
-n=${2}  # This is the variable n to control how many images to process
+dir=${1}
+output_dir=${2}
+n=${3}  # This is the variable n to control how many images to process
 
 # scale while making video from pic
 scale=0.5
@@ -8,7 +9,10 @@ IMG_PREF='img_'
 
 RNDN=$(date +%F-%H-%M-%S)-$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 5)
 
-FILE=${OUTPUT_DIR}/${RNDN}.mp4
+
+
+
+FILE=${output_dir}/${RNDN}.mp4
 
 # standard
 # framerate=20
@@ -20,7 +24,7 @@ r=25
 crf=30
 
 # Pass the variable n to limit the number of frames
-ffmpeg -framerate $framerate -i ${OUTPUT_DIR}/${IMG_PREF}%5d.png -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -frames:v $n -r $r -crf $crf ${FILE}
+ffmpeg -framerate $framerate -i ${dir}/${IMG_PREF}%5d.png -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -frames:v $n -r $r -crf $crf ${FILE}
 
 mplayer $FILE
 
